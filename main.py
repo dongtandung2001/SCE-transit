c60419a0ebd35ddf90e75ba0a236e256ef9031d4
Dung Dong <dongtandung2001@gmail.com>
2023-12-29 19:03:17 -0800

    cache

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import requests
 import typing
 import json
 from collections import defaultdict
+from datetime import datetime, timedelta
 
 from fastapi import FastAPI
 
+import pytz
+
 app = FastAPI()
 
+app.time = datetime.now()
+app.cache = None
 '''
 /predictions
 
@@ -21,6 +26,7 @@ app = FastAPI()
 '''
 API_URL = 'https://api.511.org/transit/StopMonitoring'
 API_KEY = 'edf90b7a-72f7-4f06-900d-071f76d7de62'
+
 @dataclass
 class Prediction:
     route: str
@@ -34,27 +40,36 @@ class Stop:
     predictions: typing.List[Prediction]
 
 @app.get('/predict')
-async def root():
-    results = []
-    stopIds = [63211, 63212]
-    for stopId in stopIds:
-        url = f"{API_URL}?api_key={API_KEY}&agency=SC&format=json&stopCode={stopId}"
-        res =  requests.get(url)
-        decoded_res=res.text.encode().decode('utf-8-sig') 
-        response = json.loads(decoded_res)
+async def predict():
+    print(datetime.now() - app.time)
+    if datetime.now() - app.time >= timedelta(minutes=10) or app.cache == None:
+        print('call api...')
+        results = []
+        stopIds = [63211, 63212]
+        for stopId in stopIds:
+            url = f"{API_URL}?api_key={API_KEY}&agency=SC&format=json&stopCode={stopId}"
+            res =  requests.get(url)
+            decoded_res=res.text.encode().decode('utf-8-sig') 
+            response = json.loads(decoded_res)
 
-        lines = defaultdict(list)
+            lines = defaultdict(list)
+            stops = response['ServiceDelivery']['StopMonitoringDelivery']
 
-        stops = response['ServiceDelivery']['StopMonitoringDelivery']
-        for stop in stops['MonitoredStopVisit']:
-            lines[stop['MonitoredVehicleJourney']['LineRef']].append(stop['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime'])
+            for stop in stops['MonitoredStopVisit']:
+                lines[stop['MonitoredVehicleJourney']['LineRef']] \
+                .append(stop['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime'])
 
-        id =  stops['MonitoredStopVisit'][0]['MonitoringRef']
-        name = stops['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']['StopPointName']
-        s = Stop(id, name,[])
-        
-        for line in lines:
-            s.predictions.append(Prediction(line,lines[line]))
-        results.append(s)
-    return results
+            id =  stops['MonitoredStopVisit'][0]['MonitoringRef']
+            name = stops['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']['StopPointName']
+            s = Stop(id, name,[])
+            
+            for line in lines:
+                s.predictions.append(Prediction(line,lines[line]))
+            results.append(s)
+        app.cache = results
+        app.time = datetime.now()
+        return results     
+    else:
+        print('cache used')
+        return app.cache
 
